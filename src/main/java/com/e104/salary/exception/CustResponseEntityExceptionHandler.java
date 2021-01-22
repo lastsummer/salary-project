b8335b34fb4f419f92f2d1b17cac8dae2032d104
tinghung.wang[王庭歡] <lastsummer0531@gmail.com>
2021-01-22 10:21:06 +0800

    add validation

--- src/main/java/com/e104/salary/exception/CustResponseEntityExceptionHandler.java
+++ src/main/java/com/e104/salary/exception/CustResponseEntityExceptionHandler.java
@@ -1,16 +1,20 @@
 package com.e104.salary.exception;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Date;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
 
 
 @ControllerAdvice
@@ -31,7 +35,11 @@ public class CustResponseEntityExceptionHandler  extends ResponseEntityException
 	@Override
 	protected ResponseEntity<Object> handleMethodArgumentNotValid(
 			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-		ExceptionResponse exceptionResponse= new ExceptionResponse(new Date(),ex.getMessage(), ex.getBindingResult().toString());
+		String errorMessage = "";
+        for (final ObjectError error : ex.getBindingResult().getAllErrors()) {
+        	errorMessage += ","+error.getDefaultMessage();
+        }
+		ExceptionResponse exceptionResponse= new ExceptionResponse(new Date(),errorMessage, ex.getBindingResult().toString());
 		return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
 	}
 
